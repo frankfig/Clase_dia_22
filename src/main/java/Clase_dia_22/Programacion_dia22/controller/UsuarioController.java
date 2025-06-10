5ce04e3dda063d133186e9acb51174ea488db9fc
Andrew <andrescorporativo831@gmail.com>
2025-06-10 09:50:23 -0500

    con user y usuaro2

--- src/main/java/Clase_dia_22/Programacion_dia22/controller/UsuarioController.java
+++ src/main/java/Clase_dia_22/Programacion_dia22/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -28,6 +29,10 @@ public class UsuarioController {
         return ur.findAll ();
     }
     
+    @GetMapping("/user/{id}")
+    public String getUserById(@PathVariable Integer id) {
+        return ur.findById(id).toString();
+    }
     
     
 
